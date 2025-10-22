6b6b3f8e6573f056e1c63a9571238a69747a25d6
Ariel Araya-Madrigal <58918901+Ariel100Araya@users.noreply.github.com>
2025-10-22 15:59:25 -0400

    anothercommit

--- TabTouch/extension.js
+++ TabTouch/extension.js
@@ -1,36 +1,28 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
 function activate(context) {
+  let nextTab = vscode.commands.registerCommand('extension.nextTab', () => {
+    vscode.commands.executeCommand('workbench.action.nextEditor');
+  });
+  let openCopilot = vscode.commands.registerCommand('extension.openCopilot', () => {
+    vscode.commands.executeCommand('github.copilot.chat.focus');
+  });
+  let toggleTerminal = vscode.commands.registerCommand('extension.toggleTerminal', () => {
+    vscode.commands.executeCommand('workbench.action.terminal.toggleTerminal');
+  });
+  let toggleNewWindow = vscode.commands.registerCommand('extension.newWindow', () => {
+    vscode.commands.executeCommand('workbench.action.newWindow');
+  });
+  let toggleNewUntitledFile = vscode.commands.registerCommand('extension.newUntitledFile', () => {
+    vscode.commands.executeCommand('workbench.action.newUntitledFile');
+  });
+  let prevTab = vscode.commands.registerCommand('extension.prevTab', () => {
+    vscode.commands.executeCommand('workbench.action.previousEditor');
+  });
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "TabTouch" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('TabTouch.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from TabTouch!');
-	});
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(nextTab, prevTab, openCopilot, toggleTerminal, toggleNewWindow, toggleNewUntitledFile);
 }
 
-// This method is called when your extension is deactivated
-function deactivate() {}
+  function deactivate() {}
 
-module.exports = {
-	activate,
-	deactivate
-}
+  module.exports = { activate, deactivate };
